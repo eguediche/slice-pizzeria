27a795a42c7ed83c7046f25883c383a354adad09
Ngabouja <ngabouja@edenschool.fr>
2025-05-06 17:27:01 +0200

    azz

--- js/app.js
+++ js/app.js
@@ -1,70 +1,59 @@
+async function test() {
+	const res = await fetch("http://localhost:3000/orders", {
+		method: "POST",
+		headers: {
+			Authorization: "Bearer VOTRE_TOKEN_ICI",
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			products: [
+				{ uuid: "3d684bbd-227a-45ff-b607-58eab1067598", quantity: 2 },
+				{ uuid: "fff85a88-5296-43cd-9618-cacfb3a0324b", quantity: 1 },
+			],
+		}),
+	});
+	const pizzas = await res.json();
+
+	return pizzas;
+}
+
 function renderBasketAside() {
-    const aside = document.createElement("aside");
-    aside.className = "basket-aside";
-  
-    const title = document.createElement("h2");
-    title.textContent = "Votre panier (0)";
-  
-    const emptyDiv = document.createElement("div");
-    emptyDiv.className = "empty-basket";
-  
-    const img = document.createElement("img");
-    img.src = "../images/pizza.png";
-    img.alt = "";
-  
-    const text = document.createElement("p");
-    text.textContent = "Votre panier est vide...";
-  
-    emptyDiv.appendChild(img);
-    emptyDiv.appendChild(text);
-    aside.appendChild(title);
-    aside.appendChild(emptyDiv);
-  
-    const mainWrapper = document.querySelector(".main-wrapper");
-    mainWrapper.appendChild(aside);
-  }
-  
-  const pizzas =
-    [
-        {
-            "id": "66f97356-f31c-4cc5-803a-7c42b25758e0",
-            "name": "The Godfather Pizza",
-            "description": "Gourmet Italian sausage, fresh garlic, onions & our hot giardiniera.",
-            "price": 15,
-            "image": "https://cdn.dummyjson.com/recipe-images/1.webp",
-            "category": {
-                "id": "c8755d47-4595-4768-9b0a-4cdc2fa92dd9",
-                "name": "pizza",
-                "description": "High qualities Pizza with organic ingredients and good vibes."
-            }
-        },
-        {
-            "id": "08857e40-181e-4e38-a336-22c77f532fb4",
-            "name": "The Veggie Pizza",
-            "description": "Vegetarian. Mushrooms, onions & green peppers with tomatoes on top.",
-            "price": 13,
-            "image": "https://cdn.dummyjson.com/recipe-images/1.webp",
-            "category": {
-                "id": "c8755d47-4595-4768-9b0a-4cdc2fa92dd9",
-                "name": "pizza",
-                "description": "High qualities Pizza with organic ingredients and good vibes."
-            }
-        },
-  ];
-  
-  renderBasketAside();
-  
-  function renderPizzas(pizzaList) {
-    const container = document.querySelector(".pizzas-wrapper");
-    container.innerHTML = "";
-  
-    for (let i = 0; i < pizzaList.length; i++) {
-      const pizza = pizzaList[i];
-  
-      const pizzaItem = document.createElement("div");
-      pizzaItem.classList.add("pizza-item");
-  
-      pizzaItem.innerHTML = `
+	const aside = document.createElement("aside");
+	aside.className = "basket-aside";
+
+	const title = document.createElement("h2");
+	title.textContent = "Votre panier (0)";
+
+	const emptyDiv = document.createElement("div");
+	emptyDiv.className = "empty-basket";
+
+	const img = document.createElement("img");
+	img.src = "../images/pizza.png";
+	img.alt = "";
+
+	const text = document.createElement("p");
+	text.textContent = "Votre panier est vide...";
+
+	emptyDiv.appendChild(img);
+	emptyDiv.appendChild(text);
+	aside.appendChild(title);
+	aside.appendChild(emptyDiv);
+
+	const mainWrapper = document.querySelector(".main-wrapper");
+	mainWrapper.appendChild(aside);
+}
+
+function renderPizzas(pizzaList) {
+	const container = document.querySelector(".pizzas-wrapper");
+	container.innerHTML = "";
+
+	for (let i = 0; i < pizzaList.length; i++) {
+		const pizza = pizzaList[i];
+
+		const pizzaItem = document.createElement("div");
+		pizzaItem.classList.add("pizza-item");
+
+		pizzaItem.innerHTML = `
         <img class="pizza-picture" src="${pizza.image}" alt="${pizza.name}" />
         <span class="add-to-cart-btn" name="AddToCartBtn">
           <img src="../images/carbon_shopping-cart-plus.svg" alt="" />
@@ -80,6 +69,10 @@ function renderBasketAside() {
 		container.appendChild(pizzaItem);
 	}
 }
+
+const pizzas = test();
+
+renderBasketAside();
 
 // le .then(data)  si data c l'a ou ya les donner tu change renderPizzas(data)
 renderPizzas(pizzas);
